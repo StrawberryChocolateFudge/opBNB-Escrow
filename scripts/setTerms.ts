3bbee4e4d8bcb8fdbd3a63169ed1ba6c7286c764
StrawberryChocolateFudge <hopeter@protonmail.com>
2024-11-26 11:48:13 +0100

    setting terms for opBNB

--- scripts/setTerms.ts
+++ scripts/setTerms.ts
@@ -1,23 +1,22 @@
-import {hashEscrowAgreement} from "../frontend/lib/terms";
+import { hashEscrowAgreement } from "../frontend/lib/terms";
 import { ethers } from "hardhat";
 
-const escrowAddress = "";
+//OPBNB TESTNET ADDRESS FOR MY ESCROW
+const escrowAddress = "0x30A31f1A6bD5e2b95FaAD4C6b8899886754d9B75";
 
 async function main() {
- 
- //Connect to the escrow contract and set the terms
+  //Connect to the escrow contract and set the terms
 
   const Escrow = await ethers.getContractFactory("Escrow");
 
   const escrow = await Escrow.attach(escrowAddress);
 
-  const termsHash = await hashEscrowAgreement()
+  const termsHash = await hashEscrowAgreement();
 
   await escrow.setTerms(termsHash);
 
-  console.log("Escrow deployed to:", escrow.address);
+  console.log("Terms set to ", termsHash);
 }
-
 
 main().catch((error) => {
   console.error(error);
