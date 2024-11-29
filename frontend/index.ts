31906fa08a2ec103202b5839cf06c83bfe11156c
StrawberryChocolateFudge <hopeter@protonmail.com>
2024-11-29 08:53:04 +0100

    implemented navigaton on load

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,7 +1,51 @@
 /* eslint-disable node/no-missing-import */
 // eslint-disable-next-line node/no-missing-import
 import { getPage, PageState } from "./lib/views";
+import {
+  getAgentNameByAddress,
+  getAgentRegistryContractJSONRpcProvider,
+  getArbiter,
+  getEscrowContractJSONRPC,
+  isAddress,
+} from "./lib/web3";
 
 (async function init() {
-  await getPage(PageState.connectWallet, {});
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const cparam = urlSearchParams.get("c");
+  const escrowIndex = urlSearchParams.get("i");
+
+  if (cparam === null) {
+    await getPage(PageState.AllEscrows, {});
+    return;
+  } else {
+    const validAddress = isAddress(cparam);
+    if (!validAddress) {
+      await getPage(PageState.notFound, {});
+      return;
+    }
+
+    const escrow = getEscrowContractJSONRPC(cparam);
+    const agentRegistry = getAgentRegistryContractJSONRpcProvider();
+    let agentName = "";
+    try {
+      const arbiter = await getArbiter(escrow);
+      agentName = await getAgentNameByAddress(agentRegistry, arbiter);
+    } catch (err) {
+      await getPage(PageState.notFound, {});
+      return;
+    }
+
+    if (escrowIndex === null) {
+      await getPage(PageState.connectWallet, { agentName });
+      return;
+    } else {
+      const notvalidNr = isNaN(parseInt(escrowIndex));
+      if (notvalidNr) {
+        await getPage(PageState.notFound, {});
+        return;
+      }
+
+      await getPage(PageState.connectWallet, { agentName });
+    }
+  }
 })();
